1daf99e75f1349ad661d344dcddbf6e05d1c8952
Antti Kupila <akupila@gmail.com>
2020-02-28 15:02:38 +0100

    source: add source file list

---
+++ source/filelist.go
@@ -0,0 +1,115 @@
+package source
+
+import (
+	"archive/zip"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+// A FileList contains a list of source files.
+type FileList struct {
+	root  string   // Root directory.
+	files []string // Files relative to root directory.
+}
+
+// NewFileList creates a new file list with the given root directory.
+func NewFileList(root string) *FileList {
+	return &FileList{
+		root: root,
+	}
+}
+
+// Add adds a new path to the file list. The caller is responsible for ensuring
+// the file exists. The path must be relative to the root of the file list.
+func (fl *FileList) Add(path string) {
+	if filepath.IsAbs(path) {
+		panic("Path must be relative to root")
+	}
+	fl.files = append(fl.files, path)
+}
+
+// Files returns all file names in the file list. The results are
+// lexicographically sorted.
+func (fl *FileList) Files() []string {
+	if len(fl.files) == 0 {
+		return nil
+	}
+	sort.Strings(fl.files)
+	return append([]string{}, fl.files...)
+}
+
+// Size returns the combined size of the contents of all files in bytes.
+// If an error occurs while reading the files, -1 is returned.
+func (fl *FileList) Size() int64 {
+	var size int64
+	for _, name := range fl.files {
+		stat, err := os.Stat(filepath.Join(fl.root, name))
+		if err != nil {
+			return -1
+		}
+		size += stat.Size()
+	}
+	return size
+}
+
+// Checksum computes a checksum of the contents of all the files.
+func (fl *FileList) Checksum() (string, error) {
+	s := sha256.New()
+	sort.Strings(fl.files) // Ensure consistent order
+	for _, name := range fl.files {
+		f, err := os.Open(filepath.Join(fl.root, name))
+		if err != nil {
+			return "", err
+		}
+		if _, err := io.Copy(s, f); err != nil {
+			_ = f.Close()
+			return "", err
+		}
+		_ = f.Close()
+	}
+	return hex.EncodeToString(s.Sum(nil)), nil
+}
+
+// Zip compresses the file list to a zip archive.
+func (fl *FileList) Zip(w io.Writer) error {
+	zf := zip.NewWriter(w)
+	for _, f := range fl.files {
+		if err := fl.addFileToZip(zf, f); err != nil {
+			return err
+		}
+	}
+	if err := zf.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fl *FileList) addFileToZip(z *zip.Writer, filename string) error {
+	f, err := os.Open(filepath.Join(fl.root, filename))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	info, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+	header.Name = filename
+	header.Method = zip.Deflate
+	w, err := z.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(w, f); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ source/filelist_test.go
@@ -0,0 +1,102 @@
+package source
+
+import (
+	"archive/zip"
+	"bytes"
+	"encoding/hex"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFileList(t *testing.T) {
+	files := map[string][]byte{
+		"foo.txt":     []byte("foo"),
+		"bar/baz.txt": []byte("barbaz"),
+	}
+	dir, done := writeTestFiles(t, files)
+	defer done()
+
+	fl := NewFileList(dir)
+	fl.Add("foo.txt")
+	fl.Add("bar/baz.txt")
+
+	gotFiles := fl.Files()
+	wantFiles := []string{"bar/baz.txt", "foo.txt"}
+	if diff := cmp.Diff(gotFiles, wantFiles); diff != "" {
+		t.Errorf("Files do not match (-got +want)\n%s", diff)
+	}
+
+	gotSize := fl.Size()
+	wantSize := int64(9) // len(foo + barbaz)
+	if gotSize != wantSize {
+		t.Errorf("Size does not match, got %d, want %d", gotSize, wantSize)
+	}
+
+	gotSum, err := fl.Checksum()
+	if err != nil {
+		t.Fatalf("Compute checksum: %v", err)
+	}
+	wantSum := "130b02b90cf45e7f465c717196aa4f706ab7a52901d22384d0b0562ca668598d"
+	if gotSum != wantSum {
+		t.Errorf("Checksum does not match\nGot  %q\nWant %q", gotSum, wantSum)
+	}
+
+	zip := &bytes.Buffer{}
+	if err := fl.Zip(zip); err != nil {
+		t.Fatalf("zip: %v", err)
+	}
+	checkZip(t, zip.Bytes(), files)
+}
+
+func writeTestFiles(t *testing.T, files map[string][]byte) (string, func()) {
+	t.Helper()
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for name, data := range files {
+		filename := filepath.Join(dir, name)
+		if err := os.MkdirAll(filepath.Dir(filename), 0755); err != nil {
+			t.Fatal(err)
+		}
+		if err := ioutil.WriteFile(filename, data, 0644); err != nil {
+			t.Fatal(err)
+		}
+	}
+	return dir, func() {
+		_ = os.RemoveAll(dir)
+	}
+}
+
+func checkZip(t *testing.T, data []byte, want map[string][]byte) {
+	t.Helper()
+	zf, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(zf.File) != len(want) {
+		t.Errorf("Number of files does not match; got %d, want %d", len(zf.File), len(want))
+	}
+	for _, f := range zf.File {
+		wantData, ok := want[f.Name]
+		if !ok {
+			t.Errorf("Unexpected file %q", f.Name)
+			continue
+		}
+		b, err := f.Open()
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotData, err := ioutil.ReadAll(b)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(gotData, wantData) {
+			t.Errorf("Data does not match for %s\nGot\n%s\nWant\n%s", f.Name, hex.Dump(gotData), hex.Dump(wantData))
+		}
+	}
+}
