0a0df3f23bfd95abcbd7e0d2bc8226c16074ec51
Antti Kupila <akupila@gmail.com>
2020-03-24 12:08:36 +0100

    source: add disk cache

---
+++ source/diskcache.go
@@ -0,0 +1,41 @@
+package source
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// DiskCache caches source code on disk.
+type DiskCache struct {
+	Dir string
+}
+
+// NewCache creates a new cache on disk. The cached data is stored within a
+// func directory in the user's cache directory. The path is created if needed.
+func NewDiskCache() (*DiskCache, error) {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return nil, fmt.Errorf("get user cache dir: %w", err)
+	}
+	dir := filepath.Join(cacheDir, "func")
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, fmt.Errorf("ensure application specific dir: %w", err)
+	}
+	return &DiskCache{Dir: dir}, nil
+}
+
+// Create creates a new file in cache. If the file already exists, it is truncated.
+func (d *DiskCache) Create(filename string) (*os.File, error) {
+	return os.Create(filepath.Join(d.Dir, filename))
+}
+
+// Get returns a pointer to a file in the cache. Returns nil if the file does
+// not exist.
+func (d *DiskCache) Get(filename string) *os.File {
+	f, err := os.Open(filepath.Join(d.Dir, filename))
+	if err != nil {
+		return nil
+	}
+	return f
+}
---
+++ source/diskcache_test.go
@@ -0,0 +1,49 @@
+package source
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestDiskCache(t *testing.T) {
+	cache := &DiskCache{
+		Dir: tempdir(t),
+	}
+
+	f := cache.Get("nonexisting")
+	if f != nil {
+		t.Errorf("Expected nil, got %v", f)
+	}
+
+	f, err := cache.Create("file.test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	data := []byte("hello")
+	if _, err := io.Copy(f, bytes.NewReader(data)); err != nil {
+		t.Fatal(err)
+	}
+	got, err := ioutil.ReadAll(cache.Get("file.test"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(got, data) {
+		t.Errorf("Read data does not match\nGot\n%s\nWant\n%s", hex.Dump(got), hex.Dump(data))
+	}
+}
+
+func tempdir(t *testing.T) string {
+	t.Helper()
+	dir, err := ioutil.TempDir("", "func-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(dir)
+	})
+	return dir
+}
